d8f166a574f709e2da0c1af719401ccb1629706d
liulin <2240379431@qq.com>
2021-03-10 17:10:38 +0800

    iniit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# translation-cmd
-有道翻译命令行工具
+# 安装
+
+```
+npm install yd-translation
+
+```
+
+一个结合了有道翻译的命令行工具,暂时只支持英汉互译
+
+目前可能有一些 bug 哈哈 🤣
+
+# 使用:
+
+运行 yd 单词 就可以了
+
+```
+PS D:\tran> yd name
+name
+n.名称，名字；姓名；名誉vt.命名，任命；指定；称呼；提名；叫出adj.姓名的；据以取名的abbr.(NAmE)北美英语(NorthAmericanEnglish)n.(Name)内姆（人名）；（日）滑（姓）
+
+```
---
+++ index.js
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+
+const chalk = require('chalk')
+const ora = require('ora')
+const request = require('request')
+const isChinese = require('is-chinese')
+const urlencode = require('urlencode')
+const noCase = require('no-case')
+const cheerio = require('cheerio')
+
+const spinner = ora('Loading...')
+
+let word = process.argv.slice(2).join(' ') // 获取命令行输入的参数
+
+word = noCase.noCase(word)
+
+if (!word) {
+  spinner.fail(chalk.red('Please enter text'))
+  process.exit() // Stop the process
+}
+console.log(word)
+
+const isCh = isChinese(word)
+const url = () => {
+  return isCh ? 'https://dict.youdao.com/w/eng/' : 'https://dict.youdao.com/w/'
+}
+
+const option = {
+  url: url() + urlencode(word),
+}
+
+spinner.start()
+
+request(option.url, (error, response, body) => {
+  if (error) {
+    spinner.fail(chalk.red('ERROR'))
+  } else {
+    spinner.stop()
+
+    const $ = cheerio.load(body)
+    let result = $('#phrsListTab > .trans-container > ul')
+      .text()
+      .replace(/\s+/g, '')
+
+    if (!result) {
+      // console.log(
+      //   $('#ydTrans > #fanyiToggle > .trans-container > p:nth-child(2)').text()
+      // )
+      result = $(
+        '#ydTrans > #fanyiToggle > .trans-container > p:nth-child(2)'
+      ).text()
+    }
+    console.log(chalk.green(result))
+  }
+})
